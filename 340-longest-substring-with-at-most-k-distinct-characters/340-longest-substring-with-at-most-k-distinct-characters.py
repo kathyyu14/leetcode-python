27d5f01a33afd8361c0ad9b81d54b699903e6369
Lijia Yu <kathy-ylj@hotmail.com>
2022-05-10 21:01:06 -0700

    Time: 134 ms (30.14%), Space: 14.3 MB (44.38%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,21 +1,18 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        left, right = 0,0
+        left = 0
+        res = 0
         hashMap = {}
-        max_len = 0
-        while right < len(s):
-            char = s[right]
-            hashMap[char] = hashMap.get(char, 0) + 1
-            right += 1
-
-            while len(hashMap) > k:
-                left_char = s[left]
-                print(hashMap)
-                if left_char in hashMap:
-                    hashMap[left_char] -= 1
-                if hashMap[left_char] == 0:
-                    del hashMap[left_char]
-                left += 1
-            max_len = max(max_len, right-left)
         
-        return max_len
+        for i in range(len(s)):
+            char = s[i]
+            hashMap[char] = hashMap.get(char,0)+1
+            
+            while len(hashMap) > k:
+                hashMap[s[left]] -=1
+                if hashMap[s[left]] == 0:
+                    hashMap.pop(s[left])
+                left +=1
+            res = max(res, i - left+1)
+        return res
+    
