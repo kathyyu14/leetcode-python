8ff3adb95c8a60f77bbb71bf03e3019d6ebf4862
Lijia Yu <kathy-ylj@hotmail.com>
2022-05-18 00:19:19 -0700

    Time: 1829 ms (24.77%), Space: 26.1 MB (5.02%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -6,9 +6,9 @@ class Solution:
         
         for i in range(n-1, -1, -1):
             
-            while len(stack) != 0 and temperatures[stack[-1]] <= temperatures[i]:
+            while stack and temperatures[stack[-1]] <= temperatures[i]:
                 stack.pop()
-            res[i] = 0 if len(stack) == 0 else stack[-1] - i
+            res[i] = 0 if not stack else stack[-1] - i
             stack.append(i)
         
         return res
