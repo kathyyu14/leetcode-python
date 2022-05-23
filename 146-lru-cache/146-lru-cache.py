70c6b704d10a724052918342de2772316f38167a
Lijia Yu <kathy-ylj@hotmail.com>
2022-05-23 10:09:19 -0700

    Time: 1365 ms (26.09%), Space: 76.6 MB (21.95%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,24 +1,49 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key,val
+        self.prev = self.next = None
+        
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.orderedDict = collections.OrderedDict()
-        self.capacity = capacity
+        self.cap = capacity
+        self.cache = {}
         
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+        
+    def remove(self, node): # delete from the list
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+    def insert(self, node): # insert to the right
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
 
     def get(self, key: int) -> int:
-        if key not in self.orderedDict:
-            return -1
-        self.orderedDict.move_to_end(key)
-        return self.orderedDict[key]
-        
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.orderedDict:
-            self.orderedDict.move_to_end(key)
-        self.orderedDict[key]= value
-        if len(self.orderedDict) > self.capacity:
-            self.orderedDict.popitem(0)
+        if key in self.cache:
+            # remove the node to the end
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
             
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.cap:
+            # remove from the list and delete the LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+            
+        
         
 
 
