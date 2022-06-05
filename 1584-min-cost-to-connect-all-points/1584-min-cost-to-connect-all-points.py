99b7b8c4a768d949a3366aaedccbf4b27d147a52
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-05 15:41:52 -0700

    Time: 5034 ms (14.21%), Space: 174 MB (9.89%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,20 +1,23 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        N = len(points)
-        adj = { i:[] for i in range(N) } # i : list of [cost, node]
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i + 1, N):
-                x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
+        n = len(points)
+        adj = { i:[] for i in range(n)} # i : list of [cost, node]
+        
+        # edges distances
+        for i in range(n):
+            x1,y1 = points[i]
+            for j in range(i+1, n):
+                x2,y2 = points[j]
+                dist = abs(x1-x2) + abs(y1-y2)
                 adj[i].append([dist, j])
                 adj[j].append([dist, i])
         
-        # Prim's
+        # prim's
         res = 0
         visit = set()
-        minH = [[0, 0]] # [cost, point]
-        while len(visit) < N:
+        minH = [[0,0]] # [cost, point]
+        
+        while len(visit) < n:
             cost, i = heapq.heappop(minH)
             if i in visit:
                 continue
@@ -23,4 +26,5 @@ class Solution:
             for neiCost, nei in adj[i]:
                 if nei not in visit:
                     heapq.heappush(minH, [neiCost, nei])
+        
         return res
