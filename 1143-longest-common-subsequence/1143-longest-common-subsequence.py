8a2392fe381894778212df9b6201149f60ae3285
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-15 16:57:01 -0700

    Time: 405 ms (89.69%), Space: 21.9 MB (76.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,10 +5,11 @@ class Solution:
         
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                # 现在 i 和 j 从 1 开始，所以要减一
-                if text1[i-1] == text2[j-1]:
-                    # s1[i-1] 和 s2[j-1] 必然在 lcs 中
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
         return dp[m][n]
+        
+        
