bf8a3c1b7e8d9c81518a9dc9ae2e837ebaa28b1b
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-15 16:35:42 -0700

    Time: 58 ms (21.02%), Space: 14.1 MB (15.23%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,15 +2,19 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         memo = [[0] * n for _ in range(m)]
         
-        def dp(x, y):
-            if x == 0 and y == 0:
+        def dp(i, j):
+            if i == 0 or j == 0:
                 return 1
-            if x < 0 or y < 0:
-                return 0
-            if memo[x][y] > 0:
-                return memo[x][y]
             
-            memo[x][y] = dp(x - 1, y) + dp(x, y - 1)
-            return memo[x][y]
+            if i < 0 or j < 0 or i >= m or j >= n:
+                return 0
+            
+            if memo[i][j] > 0:
+                return memo[i][j]
+            
+            memo[i][j] = dp(i - 1, j) + dp(i, j - 1)
+            
+            return memo[i][j]
         
-        return dp(m - 1, n - 1)
+        return dp(m-1, n-1)
+            
