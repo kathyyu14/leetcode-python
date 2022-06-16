0667118d5a5c8c10c00638912af7c5e2629a2273
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-16 11:36:06 -0700

    Time: 281 ms (26.41%), Space: 15.3 MB (78.52%) - LeetHub

---
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxValue(self, n: str, x: int) -> str:
+        i = 0
+        
+        if n[0] == "-":
+            i += 1
+            while i < len(n) and int(n[i]) <= x:
+                i += 1
+        else:
+            while i < len(n) and int(n[i]) >= x:
+                i += 1
+        
+        return n[:i] + str(x) + n[i:]
+    
