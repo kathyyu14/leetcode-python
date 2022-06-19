f924480871c951dea5209b8f413ca01a6363bde7
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-19 16:59:28 -0700

    Time: 332 ms (10.84%), Space: 15.6 MB (34.53%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num] = 1
+            hashmap[num] += 1
+        
+        
+        maxNum = 0
+        res = 0
+        
+        for idx, value in hashmap.items():
+            if value > maxNum:
+                res = idx
+                maxNum = value
+        
+        return res
+            
