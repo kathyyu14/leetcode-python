7bb85abc34a98ef790f80a45071a43182d0d5b9e
Lijia Yu <kathy-ylj@hotmail.com>
2022-06-19 17:01:05 -0700

    Time: 164 ms (97.61%), Space: 15.4 MB (85.75%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,19 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
-        for num in nums:
-            if num not in hashmap:
-                hashmap[num] = 1
-            hashmap[num] += 1
+#         hashmap = {}
+#         for num in nums:
+#             if num not in hashmap:
+#                 hashmap[num] = 1
+#             hashmap[num] += 1
         
         
-        maxNum = 0
-        res = 0
+#         maxNum = 0
+#         res = 0
         
-        for idx, value in hashmap.items():
-            if value > maxNum:
-                res = idx
-                maxNum = value
+#         for idx, value in hashmap.items():
+#             if value > maxNum:
+#                 res = idx
+#                 maxNum = value
         
-        return res
-            
+#         return res
+        
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
